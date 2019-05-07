b11f10d05e6db888aadda3be9ab6d50c4ff229f7
Volker Richter <vrichter@home.intern.coderain.de>
2019-05-07 18:18:20 +0200

    updated to support login - based inverters

--- build/main.js
+++ build/main.js
@@ -17,9 +17,14 @@ const utils = require("@iobroker/adapter-core");
 const request = require("request");
 const SMAObjects_1 = require("./SMAObjects");
 const util_1 = require("util");
+//require("request-debug")(request);
+let _self = this;
 class SmaSview extends utils.Adapter {
     constructor(options = {}) {
         super(Object.assign({}, options, { name: "sma-sview" }));
+        this.sessionId = "";
+        this.loggedIn = false;
+        _self = this;
         this.on("ready", this.onReady.bind(this));
         //	this.on("objectChange", this.onObjectChange.bind(this));
         //	this.on("stateChange", this.onStateChange.bind(this));
@@ -31,56 +36,142 @@ class SmaSview extends utils.Adapter {
      */
     onReady() {
         return __awaiter(this, void 0, void 0, function* () {
-            // Initialize your adapter here
-            // The adapters config (in the instance object everything under the attribute "native") is accessible via
-            // this.config:
-            this.log.info("Inverter IP: " + this.config.smartViewIP);
-            this.log.info("Rescan Interval: " + this.config.rescanInterval);
-            this.requestJSONFromInverter();
+            this.log.info("Inverter IP: " + this.config.inverterIP);
+            request.defaults({
+                jar: true,
+                gzip: true,
+                headers: {
+                    "Referer": "http://" + this.config.inverterIP + "/",
+                    "Accept-Language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+                    "Content-Type": "application/json;charset=UTF-8",
+                    "Accept": "application/json, text/plain, */*"
+                }
+            });
+            const requestFilename = (this.config.smartViewEnabled) ? SmaSview.JSON_SMARTVIEW : SmaSview.JSON_VALUES;
+            if (this.config.smartViewEnabled) {
+                this.requestJSONFromInverter();
+            }
+            else {
+                if (this.config.loginNeeded == false) {
+                    this.log.warn("You need to enable login if smartview is disabled");
+                    return;
+                }
+                this.log.debug("Fetching Cookie");
+                this.snackCookie().then(() => {
+                    this.tryLogin().then(() => {
+                        this.log.debug("Requesting JSON " + requestFilename + " with session " + this.sessionId);
+                        this.requestJSONFromInverter(requestFilename, "http", this.sessionId);
+                    }).catch((reason) => {
+                        this.log.warn("Login not successful: " + reason + " Trying again in 30s");
+                        setTimeout(() => {
+                            this.onReady.bind(this);
+                        }, 30000);
+                    });
+                }).catch((reason) => {
+                    this.log.warn("Connection failed: " + reason);
+                });
+            }
         });
     }
-    requestJSONFromInverter() {
-        process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = "0";
-        request("https://" + this.config.smartViewIP + "/dyn/getDashValues.json", { method: 'POST', formData: { destDev: [], keys: [] } }, (error, response, body) => {
-            if (error) {
-                this.log.error(error);
-                return;
-            }
-            this.log.debug("Status: " + response.statusCode);
-            if (response.statusCode == 200) {
-                let obj = JSON.parse(body);
-                if (!obj) {
-                    this.log.error("Parsing JSON failed: " + body);
+    snackCookie() {
+        _self.log.debug("Getting cookie");
+        return new Promise((resolve, reject) => {
+            _self.log.debug("Requesting " + _self.config.inverterIP);
+            request("http://" + _self.config.inverterIP + "/", (error, response, body) => {
+                if (error) {
+                    reject("failed to connect");
                     return;
                 }
-                if (obj.result) {
-                    for (let t in obj.result) {
-                        this.log.debug("First Key: " + t);
-                        const states = this.parseResult(obj.result[t]);
-                        this.createStates(states, () => {
-                            setTimeout(() => { this.requestJSONFromInverter(); }, this.config.rescanInterval);
-                        });
+                resolve();
+            });
+        });
+    }
+    tryLogin() {
+        return new Promise((resolve, reject) => {
+            this.log.debug("requesting " + "http://" + this.config.inverterIP + "/dyn/" + SmaSview.JSON_LOGIN + ".json");
+            request.post({
+                url: "http://" + this.config.inverterIP + "/dyn/" + SmaSview.JSON_LOGIN + ".json",
+                form: JSON.stringify({
+                    "right": this.config.loginUser,
+                    "pass": this.config.loginPassword
+                })
+            }, (error, response, body) => {
+                if (error) {
+                    this.log.error(error);
+                    reject(error);
+                    return;
+                }
+                if (response.statusCode == 200) {
+                    const res = JSON.parse(body);
+                    if (res.result) {
+                        if (res.result.sid) {
+                            this.loggedIn = true;
+                            this.sessionId = res.result.sid;
+                            this.log.debug("Logged in with Session ID: " + this.sessionId);
+                            resolve();
+                            return;
+                        }
                     }
                 }
-                else {
-                    this.log.error("Result Object not found in : " + JSON.stringify(obj));
+                this.loggedIn = false;
+                this.sessionId = "";
+                reject("unsuccessful: " + body);
+            });
+        });
+    }
+    requestJSONFromInverter(jsonFile = SmaSview.JSON_SMARTVIEW, schema = "https", sid = "") {
+        _self.log.debug("this is: ");
+        _self.log.debug(typeof this);
+        process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = "0";
+        return new Promise((resolve, reject) => {
+            const url = schema + "://" + _self.config.inverterIP + "/dyn/" + jsonFile + ".json?sid=" + sid;
+            _self.log.debug("Requesting: " + url);
+            request(url, { method: 'POST', form: JSON.stringify({ destDev: [], keys: [] }) }, (error, response, body) => {
+                if (error) {
+                    _self.log.error(error);
+                    reject(error);
                     return;
                 }
-            }
+                _self.log.debug("Status: " + response.statusCode);
+                if (response.statusCode == 200) {
+                    let obj = JSON.parse(body);
+                    if (!obj) {
+                        _self.log.error("Parsing JSON failed: " + body);
+                        reject("JSON");
+                        return;
+                    }
+                    if (obj.result) {
+                        for (let t in obj.result) {
+                            _self.log.debug("First Key: " + t);
+                            const states = this.parseResult(obj.result[t]);
+                            this.createStates(states, () => {
+                                setTimeout(() => { _self.requestJSONFromInverter(jsonFile, schema, sid); }, _self.config.rescanInterval);
+                                resolve({});
+                            });
+                        }
+                    }
+                    else {
+                        _self.log.error("Result Object not found in : " + JSON.stringify(obj));
+                        reject("No result object");
+                        return;
+                    }
+                }
+            });
         });
     }
     createStates(states, callback) {
         if (!states || states.length == 0) {
-            this.log.warn("States Array is empty.");
+            _self.log.warn("States Array is empty.");
             return;
         }
         for (let t in states) {
-            this.log.debug("Setting state " + t + " to " + states[t]);
+            _self.log.debug("Setting state " + t + " to " + states[t]);
             let typ = (util_1.isNumber(states[t])) ? "value" : "string";
-            this.getObjectAsync(t).then(() => {
-                this.setState(t, states[t], true);
+            _self.getObjectAsync(t).then((g) => {
+                _self.setState(t, states[t], true);
             }).catch((reason) => {
-                this.setObjectAsync(t, {
+                _self.setObjectAsync(t, {
                     type: "state",
                     common: {
                         name: t,
@@ -91,7 +182,7 @@ class SmaSview extends utils.Adapter {
                     },
                     native: {},
                 }).then(() => {
-                    this.setState(t, states[t], true);
+                    _self.setState(t, states[t], true);
                 });
             });
         }
@@ -101,16 +192,18 @@ class SmaSview extends utils.Adapter {
         let states = {};
         for (var l in obj) {
             if (SMAObjects_1.SMAObjects.OBJECTS[l]) {
-                for (var o in obj[l]["1"]) {
-                    if (obj[l]["1"][o].val) {
-                        states[SMAObjects_1.SMAObjects.OBJECTS[l]] = obj[l]["1"][o].val;
-                        // if is an array with a tag
-                        if (!states[SMAObjects_1.SMAObjects.OBJECTS[l]].length)
-                            continue;
-                        const tag = states[SMAObjects_1.SMAObjects.OBJECTS[l]][0].tag;
-                        if (tag) {
-                            if (SMAObjects_1.D2PConst[tag]) {
-                                states[SMAObjects_1.SMAObjects.OBJECTS[l]] = SMAObjects_1.D2PConst[tag].toString();
+                for (var x in obj[l]) {
+                    for (var o in obj[l][x]) {
+                        if (obj[l][x][o].val) {
+                            states[SMAObjects_1.SMAObjects.OBJECTS[l]] = obj[l][x][o].val;
+                            // if is an array with a tag
+                            if (!states[SMAObjects_1.SMAObjects.OBJECTS[l]].length)
+                                continue;
+                            const tag = states[SMAObjects_1.SMAObjects.OBJECTS[l]][0].tag;
+                            if (tag) {
+                                if (SMAObjects_1.D2PConst[tag]) {
+                                    states[SMAObjects_1.SMAObjects.OBJECTS[l]] = SMAObjects_1.D2PConst[tag].toString();
+                                }
                             }
                         }
                     }
@@ -132,6 +225,9 @@ class SmaSview extends utils.Adapter {
         }
     }
 }
+SmaSview.JSON_SMARTVIEW = "getDashValues";
+SmaSview.JSON_VALUES = "getAllOnlValues";
+SmaSview.JSON_LOGIN = "login";
 if (module.parent) {
     // Export the constructor in compact mode
     module.exports = (options) => new SmaSview(options);
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@
 // you need to create an adapter
 import * as utils from "@iobroker/adapter-core";
 import * as request from "request";
+
+
 import {SMAObjects, D2PConst} from "./SMAObjects";
 import { isNumber } from "util";
+import { Agent } from "http";
 
+//require("request-debug")(request);
+let _self : any     = this;
 // Load your modules here, e.g.:
 // import * as fs from "fs";
 
@@ -18,8 +23,12 @@ declare global {
 	namespace ioBroker {
 		interface AdapterConfig {
 			// Define the shape of your options here (recommended)
-			smartViewIP: string;
+			inverterIP: string;
 			rescanInterval: number;
+			smartViewEnabled: boolean;
+			loginNeeded: boolean;
+			loginUser: string;
+			loginPassword: string;
 			// Or use a catch-all approach
 			[key: string]: any;
 		}
@@ -28,84 +37,184 @@ declare global {
 
 class SmaSview extends utils.Adapter {
 
+	static JSON_SMARTVIEW: string = "getDashValues";
+	static JSON_VALUES: string = "getAllOnlValues";
+	static JSON_LOGIN: string = "login";
+
+	sessionId : string = "";
+	loggedIn : boolean = false;
+	
 	constructor(options: Partial<ioBroker.AdapterOptions> = {}) {
 		super({
 			...options,
 			name: "sma-sview",
 		});
+		_self = this;
 		this.on("ready", this.onReady.bind(this));
 	//	this.on("objectChange", this.onObjectChange.bind(this));
 	//	this.on("stateChange", this.onStateChange.bind(this));
 		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
+			
 	}
 
 	/**
 	 * Is called when databases are connected and adapter received configuration.
 	 */
 	private async onReady() {
-		// Initialize your adapter here
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("Inverter IP: " + this.config.smartViewIP);
-		this.log.info("Rescan Interval: " + this.config.rescanInterval);
+		this.log.info("Inverter IP: " + this.config.inverterIP);
+		request.defaults({
+			jar: true,
+			gzip: true,
+			headers: {
+				"Referer": "http://"+this.config.inverterIP+"/",
+				"Accept-Language" : "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+				"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+				"Content-Type" : "application/json;charset=UTF-8",
+				"Accept" : "application/json, text/plain, */*"
+			}});
 
+		const requestFilename = (this.config.smartViewEnabled) ? SmaSview.JSON_SMARTVIEW : SmaSview.JSON_VALUES;
+
+		if(this.config.smartViewEnabled) {
+			this.requestJSONFromInverter();
+		} else {
+			if(this.config.loginNeeded == false) {
+				this.log.warn("You need to enable login if smartview is disabled");
+				return;
+			}
+			this.log.debug("Fetching Cookie");
+			this.snackCookie(). then(() => {
+				
+				this.tryLogin().then(() => {
+					this.log.debug("Requesting JSON " + requestFilename + " with session " + this.sessionId);
+					this.requestJSONFromInverter(requestFilename, "http", this.sessionId);	
+				}).catch((reason) => {
+					this.log.warn("Login not successful: " + reason + " Trying again in 30s");
+					setTimeout(() => {
+						this.onReady.bind(this);
+					}, 30000);
+				});
+			}).catch((reason)=> {
+				this.log.warn("Connection failed: " + reason);
+			});
+			
+		}
 		
-
-		
-		this.requestJSONFromInverter();
 		
 	}
-	private requestJSONFromInverter(){
-		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = "0";
-		 
-		request("https://" + this.config.smartViewIP + "/dyn/getDashValues.json", { method: 'POST', formData: {destDev: [], keys: []}}, (error: any, response: request.Response, body: any) => {
-				if(error) {
-					this.log.error(error);
+	private snackCookie(): Promise<void> {
+		_self.log.debug("Getting cookie");
+		return new Promise<void>((resolve, reject) => {
+			_self.log.debug("Requesting " + _self.config.inverterIP);
+			request("http://"+_self.config.inverterIP + "/", (error: any, response: request.Response, body: any) => {
+				if(error){
+					reject("failed to connect");
 					return;
 				}
-				this.log.debug("Status: " + response.statusCode);
+					
+				resolve();
+				
+			});
+			
+		});
+	}
+	private tryLogin(): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			this.log.debug("requesting " + "http://" + this.config.inverterIP + "/dyn/" + SmaSview.JSON_LOGIN + ".json");
+			
+			request.post(
+			{ 
+				url: "http://" + this.config.inverterIP + "/dyn/" + SmaSview.JSON_LOGIN + ".json",
+				form: JSON.stringify({
+					"right": this.config.loginUser,
+					"pass": this.config.loginPassword
+				})
+			}, (error: any, response: request.Response, body: any) => {
+				if(error) {
+					this.log.error(error);
+					reject(error);
+
+					return;
+				}
+				if(response.statusCode == 200) {
+					const res = JSON.parse(body);
+					
+					if(res.result) {
+						if(res.result.sid) {
+							this.loggedIn = true;
+							this.sessionId = res.result.sid;
+							this.log.debug("Logged in with Session ID: " + this.sessionId);
+							resolve();
+							
+							return;
+						}
+					} 
+				}
+				this.loggedIn = false;
+				this.sessionId = "";
+				reject("unsuccessful: " + body);
+			})
+		});
+		
+	}
+	requestJSONFromInverter (jsonFile: string = SmaSview.JSON_SMARTVIEW, schema: string = "https", sid: String = "") : Promise<object> {
+		_self.log.debug("this is: ");
+		_self.log.debug(typeof this);
+		process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = "0";
+		return new Promise<object>((resolve, reject) => {
+			const url = schema + "://" + _self.config.inverterIP + "/dyn/"+ jsonFile + ".json?sid="+sid;
+			_self.log.debug("Requesting: "+ url);
+			request(url, { method: 'POST', form: JSON.stringify({destDev: [], keys: []})}, (error: any, response: request.Response, body: any) => {
+				if(error) {
+					_self.log.error(error);
+					reject(error);
+					return;
+				}
+				_self.log.debug("Status: " + response.statusCode);
 				if(response.statusCode == 200){
 					let obj = JSON.parse(body);
 					
 					if(!obj) {
-						this.log.error("Parsing JSON failed: " + body);
-
+						_self.log.error("Parsing JSON failed: " + body);
+						reject("JSON");
 						return;
 					}
 					if(obj.result) {
 						for(let t in obj.result) {
-							this.log.debug("First Key: " + t);
+							_self.log.debug("First Key: " + t);
 
 							const states = this.parseResult(obj.result[t]);
 							this.createStates(states, () => {
-								setTimeout(() => { this.requestJSONFromInverter(); }, this.config.rescanInterval);
+								setTimeout(() => { _self.requestJSONFromInverter(jsonFile, schema, sid); }, _self.config.rescanInterval);
+								resolve({});
 							});
 						}
 					} else {
-						this.log.error("Result Object not found in : " + JSON.stringify(obj));
-
+						_self.log.error("Result Object not found in : " + JSON.stringify(obj));
+						reject("No result object");
 						return;
 					}
 				}
 
 				
 		});
+		});
 }
 createStates(states: any, callback: Function) {
 	if(!states || states.length == 0){
-		this.log.warn("States Array is empty.");
+		_self.log.warn("States Array is empty.");
 		return;
 	}
 
 	for(let t in states) {
-		this.log.debug("Setting state " + t + " to " + states[t]);
+		_self.log.debug("Setting state " + t + " to " + states[t]);
 		let typ = (isNumber(states[t])) ? "value" : "string";
-		this.getObjectAsync(t).then(() => {
-			this.setState(t, states[t], true);
+		_self.getObjectAsync(t).then((g:any) => {
+			_self.setState(t, states[t], true);
 		}).catch((reason:any) => {
-			this.setObjectAsync(t, {
+			_self.setObjectAsync(t, {
 				type: "state",
 				common: {
 					name: t,
@@ -118,19 +227,21 @@ createStates(states: any, callback: Function) {
 				
 				native: {},
 			}).then(()=>{
-				this.setState(t, states[t], true);
+				_self.setState(t, states[t], true);
 			});
 		});
 	}
 	callback();
 }
 parseResult(obj: any) : any {
+	
 	let states : any= {}; 
 	for ( var l in obj) {
 		if(SMAObjects.OBJECTS[l]) {
-			for( var o in obj[l]["1"]){
-				if(obj[l]["1"][o].val){
-					states[SMAObjects.OBJECTS[l]] = obj[l]["1"][o].val;
+			for(var x in obj[l]){
+			for( var o in obj[l][x]){
+				if(obj[l][x][o].val){
+					states[SMAObjects.OBJECTS[l]] = obj[l][x][o].val;
 
 					// if is an array with a tag
 					if(!states[SMAObjects.OBJECTS[l]].length) 
@@ -146,6 +257,7 @@ parseResult(obj: any) : any {
 				}
 				
 			}
+		}
 			
 		}
 	}
